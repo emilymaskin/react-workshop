031ab7cb60168e89a3b9ff9e02dd84c35e3c9f0f
Emily Maskin <emily.maskin@gmail.com>
2017-05-16 14:00:48 +0200

    with min length validation

--- src/withMinLengthValidation.js
+++ src/withMinLengthValidation.js
@@ -1,1 +1,21 @@
 import React from 'react';
+
+const withMinLengthValidation = (Component, length) => {
+  return (props) => {
+    const {
+      value,
+      onChange,
+    } = props;
+
+    const valueLength = value.toString().length;
+
+    return (
+      <div>
+        <Component value={value} onChange={onChange} />
+        {valueLength < length && <span>Minimum {length} required.</span>}
+      </div>
+    );
+  }
+}
+
+export default withMinLengthValidation;
