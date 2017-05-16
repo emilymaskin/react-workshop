23563ac7dfd09978e4d4e72f32f08ebf26767abe
Emily Maskin <emily.maskin@gmail.com>
2017-05-16 14:23:02 +0200

    or, their solution, i think

--- src/withDefaultCountProp.js
+++ src/withDefaultCountProp.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 const withDefaultCountPropNine = (Counter, defaultCount) => {
   return ({count}) => <Counter count={count || defaultCount} />
+  //return (props) => <Counter count={defaultCount} {...props} />
 }
 
 export default withDefaultCountPropNine;
