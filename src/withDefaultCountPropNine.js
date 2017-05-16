7afa46c29da477e70fe4748018e1b053a7721fc3
Emily Maskin <emily.maskin@gmail.com>
2017-05-16 14:21:42 +0200

    or, their solution

--- src/withDefaultCountPropNine.js
+++ src/withDefaultCountPropNine.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 const withDefaultCountPropNine = (Counter) => {
   return ({count}) => <Counter count={count || 9} />
+  //return (props) => <Counter count={9} {...props} />
 }
 
 export default withDefaultCountPropNine;
